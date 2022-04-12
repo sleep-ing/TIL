bca1a6411b26b804a6684609df3ee80dd011358c
sleep-ing <communionyung@gmail.com>
2022-04-12 20:51:58 +0900

    Users List 갱신

--- React/connect_API/api-integrate/src/Users.js
+++ React/connect_API/api-integrate/src/Users.js
@@ -1,44 +1,38 @@
 import React, { useState } from 'react';
-import axios from 'axios';
-import { useAsync } from 'react-async';
+import { useUsersState, useUsersDispatch, getUsers } from './UsersContext';
 import User from './User';
 
-const getUsers = async() => {
-    const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/users'
-    );
-    return response.data;
-}
+function Users() {
+  const [userId, setUserId] = useState(null);
+  const state = useUsersState();
+  const dispatch = useUsersDispatch();
 
+  const { data: users, loading, error } = state.users;
+  const fetchData = () => {
+    getUsers(dispatch);
+  };
 
-const Users = () => {
-    const [userId, setUserId] = useState(null);
-    const {data: users, error, isLoading, run} = useAsync({
-        deferFn: getUsers
-    });
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
+  if (!users) return <button onClick={fetchData}>불러오기</button>;
 
-    if (isLoading) return <div>로딩중...</div>
-    if (error) return <div>에러가 발생했습니다</div>
-    if (!users) return <button onClick={run}>불러오기</button>
-
-    return (
-        <>
-            <ul>
-                {users.map(user => (
-                    <li
-                        key={user.id}
-                        onClick={() => setUserId(user.id)}
-                        style={{ cursor: 'pointer'}}
-                    >
-                        {user.username} ({user.name})
-                    </li>
-                ))}
-            </ul>
-            <button onClick={run}>다시 불러오기</button>
-            {userId && <User id={userId} /> }
-        </>
-    );
-
+  return (
+    <>
+      <ul>
+        {users.map(user => (
+          <li
+            key={user.id}
+            onClick={() => setUserId(user.id)}
+            style={{ cursor: 'pointer' }}
+          >
+            {user.username} ({user.name})
+          </li>
+        ))}
+      </ul>
+      <button onClick={fetchData}>다시 불러오기</button>
+      {userId && <User id={userId} />}
+    </>
+  );
 }
 
 export default Users;
