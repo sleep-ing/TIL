8974d58a9fcc02517f7d5570db0ffad446ce8fc9
sleep-ing <communionyung@gmail.com>
2022-04-12 18:08:34 +0900

    update: react-async사용

--- React/connect_API/api-integrate/src/Users.js
+++ React/connect_API/api-integrate/src/Users.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import useAsync from './useAsync';
+import { useAsync } from 'react-async';
 import User from './User';
 
 const getUsers = async() => {
@@ -13,13 +13,13 @@ const getUsers = async() => {
 
 const Users = () => {
     const [userId, setUserId] = useState(null);
-    const [state, refetch] = useAsync(getUsers, [], true);
+    const {data: users, error, isLoading, run} = useAsync({
+        deferFn: getUsers
+    });
 
-    const {loading, data: users, error} = state;
-
-    if (loading) return <div>로딩중...</div>
+    if (isLoading) return <div>로딩중...</div>
     if (error) return <div>에러가 발생했습니다</div>
-    if (!users) return <button onClick={refetch}>불러오기</button>
+    if (!users) return <button onClick={run}>불러오기</button>
 
     return (
         <>
@@ -34,7 +34,7 @@ const Users = () => {
                     </li>
                 ))}
             </ul>
-            <button onClick={refetch}>다시 불러오기</button>
+            <button onClick={run}>다시 불러오기</button>
             {userId && <User id={userId} /> }
         </>
     );
